d85e9f71ff79c6127cafa4c0ccf9253308c38eb6
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-03-31 13:00:22 +0530

    Update and rename String Split and Join to StringSplitandJoin.py
    added other approach

--- StringSplitandJoin.py
+++ StringSplitandJoin.py
@@ -5,3 +5,10 @@ def split_and_join(line):
     return line
 
 if __name__ == '__main__':
+    
+#     or this approach is also apt
+
+def split_and_join(line):
+    return line.replace(" ","-")
+
+if __name__ == '__main__':
