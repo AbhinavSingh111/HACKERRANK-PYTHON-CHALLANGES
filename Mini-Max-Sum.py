a144e48c610b75feeedefa4d4395697ccc2581b4
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-10-28 23:48:22 +0530

    Create Mini-Max-Sum.py

---
+++ Mini-Max-Sum.py
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+# Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+# easy and simple python approach:
+
+def miniMaxSum(arr):
+    # Write your code here
+    maximum_sum = -float('inf')
+    minimum_sum = float('inf')
+    total_sum = sum(arr)
+    for i in arr:
+        temp_sum = total_sum-i
+        if temp_sum>maximum_sum:
+            maximum_sum = temp_sum
+        if temp_sum<minimum_sum:
+            minimum_sum = temp_sum
+    print(int(minimum_sum) , int(maximum_sum))
+    
+#     2 liner , easy and simple python approach:
+
