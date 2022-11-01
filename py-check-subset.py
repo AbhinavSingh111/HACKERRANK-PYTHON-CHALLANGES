526394af53e68874e5be1195a8cbb2c0f6a30808
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-11-01 23:56:22 +0530

    Update py-check-subset.py

--- py-check-subset.py
+++ py-check-subset.py
@@ -7,6 +7,9 @@ for i in range(n_t_c):
     n_b = int(input())
     s_b = set(input().split())
     if len(s_a) == len(s_a.intersection(s_b)):
+    # if len(s_a.difference(s_b))==0:
+    # if len(s_a.union(s_b))==len(s_b):    # all 3 conditions are working
+#     if s_a.issubset(s_b):
         print("True")
     else:
         print("False")
