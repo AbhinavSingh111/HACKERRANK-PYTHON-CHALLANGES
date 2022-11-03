c84c72c61498093d49bcfedbaf4efb265f4a7c54
Abhinav Singh <81260957+AbhinavSingh111@users.noreply.github.com>
2022-11-04 00:50:47 +0530

    Update py-the-captains-room.py

--- py-the-captains-room.py
+++ py-the-captains-room.py
@@ -11,3 +11,18 @@ for i in range(len(r_n)):
 for key,values in d.items():
     if values==1:
         print(key)
+        
+#      or
+
+k = int(input())
+r_n = input().split()
+r_s = set(r_n)
+for i in r_s:
+    r_n.remove(i)
+# res = list(r_s.difference(r_n))
+# print(int(res[0]))
+
+# or
+
+print(*(r_s.difference(r_n)))
+    
